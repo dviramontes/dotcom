9bea558142cca0e740d031deb3a639ed471c93ad
David A. Viramontes <dviramontes@gmail.com>
2022-07-27 14:52:48 -0400

    chore: format, wip: update images and project details

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { BLOG_PATH } from '../lib/constants'
 
 const Footer = () => {
   return (
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            A blog by David A. Viramontes
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -17,7 +17,7 @@ const Footer = () => {
               Read Documentation
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://github.com/dviramontes/${BLOG_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
