d4ca1a427070e150d8f3c31e56b0cd387a616f3e
David Viramontes <dviramontes@gmail.com>
2025-04-02 21:56:23 -0600

    refactor: improve footer layout with responsive grid and consistent spacing

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,38 +12,52 @@ const Footer = () => {
             </Link>
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/3">
-            <Link href="/about">
-              <span className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
-                About
-              </span>
-            </Link>
-            <Link href="/">
-              <span className="mx-3 font-bold hover:underline">Blog</span>
-            </Link>
-            <Link href="/til">
-              <span className="mx-3 font-bold hover:underline">TIL</span>
-            </Link>
-            <a
-              target="_blank"
-              href="https://github.com/dviramontes"
-              className="mx-3 font-bold hover:underline"
-            >
-              GitHub
-            </a>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/davidviramontes/"
-              className="mx-3 font-bold hover:underline"
-            >
-              Linkedin
-            </a>
-            <a
-              target="_blank"
-              href="https://dviramontes.bsky.social/"
-              className="mx-3 font-bold hover:underline"
-            >
-              Bluesky
-            </a>
+            <div className="grid grid-cols-3 gap-y-6 gap-x-2 lg:flex lg:flex-row w-full lg:space-x-2 items-center px-4 lg:px-0">
+              <div className="w-full lg:w-auto my-2 lg:my-0 text-center">
+                <Link href="/about">
+                  <span className="bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-8 lg:px-8 duration-200 transition-colors">
+                    About
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full lg:w-auto my-2 lg:my-0 text-center">
+                <Link href="/">
+                  <span className="font-bold hover:underline">Blog</span>
+                </Link>
+              </div>
+              <div className="w-full lg:w-auto my-2 lg:my-0 text-center">
+                <Link href="/til">
+                  <span className="font-bold hover:underline">TIL</span>
+                </Link>
+              </div>
+              <div className="w-full lg:w-auto my-2 lg:my-0 text-center">
+                <a
+                  target="_blank"
+                  href="https://github.com/dviramontes"
+                  className="font-bold hover:underline"
+                >
+                  GitHub
+                </a>
+              </div>
+              <div className="w-full lg:w-auto my-2 lg:my-0 text-center">
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/davidviramontes/"
+                  className="font-bold hover:underline"
+                >
+                  Linkedin
+                </a>
+              </div>
+              <div className="w-full lg:w-auto my-2 lg:my-0 text-center">
+                <a
+                  target="_blank"
+                  href="https://dviramontes.bsky.social/"
+                  className="font-bold hover:underline"
+                >
+                  Bluesky
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
