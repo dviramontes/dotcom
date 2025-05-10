dfc9631d23bd05a7e6df9ec092eaed190c05cc95
David Viramontes <dviramontes@gmail.com>
2025-05-10 14:25:14 -0400

    improve date display on posts

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -7,8 +7,25 @@ type Props = {
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString)
   return (
-    <time className="text-sm" dateTime={dateString}>
-      {format(date, 'yyyy-MM-dd')}
+    <time
+      className="inline-flex items-center px-3 py-1 rounded-full bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 text-base font-medium"
+      dateTime={dateString}
+    >
+      <svg
+        className="w-4 h-4 mr-2"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+        />
+      </svg>
+      {format(date, 'MMMM d, yyyy')}
     </time>
   )
 }
