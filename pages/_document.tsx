951f3d1dcbc7a7ca7527ba034e877d9dec456215
David Viramontes <dviramontes@gmail.com>
2026-04-26 17:59:13 -0400

    Add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,12 +17,22 @@ const themeScript = `
   })()
 `
 
+const plausibleScript = `
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+`
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
           <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+          <script
+            async
+            src="https://dotcom-analytics.exe.xyz/js/pa-gfgiDC091HW63G4-E4RvG.js"
+          />
+          <script dangerouslySetInnerHTML={{ __html: plausibleScript }} />
         </Head>
         <body>
           <Main />
